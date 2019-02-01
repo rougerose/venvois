ae5863903bf9c24a62a7edcf064a62438809c848
christophe le drean <christophe@rouge-rose.net>
2019-02-02 00:47:39 +0100

    Corrections sur export envois ponctuels

--- action/exporter_envois_ponctuel.php
+++ action/exporter_envois_ponctuel.php
@@ -5,20 +5,23 @@ if (!defined("_ECRIRE_INC_VERSION")) {
 }
 
 
-function action_exporter_envois_ponctuel($id_objet = null, $objet = null) {
+function action_exporter_envois_ponctuel($arg = null) {
 	// appel direct depuis une url avec arg = "id-objet-objet"
-	if (is_null($id_objet) or is_null($objet)) {
+	if (is_null($arg)) {
 		$securiser_action = charger_fonction('securiser_action', 'inc');
 		$arg = $securiser_action();
-		list($id_objet, $objet) = explode("-", $arg);
 	}
-	
+
+	list($objet, $id_objet) = explode("-", $arg);
+
+	$id_objet = intval($id_objet);
+
 	if ($id_objet and $objet and $objet = 'envois_ponctuel') {
 		// Les auteurs à exporter
-		$auteurs = sql_allfetsel('objet, id_objet, quantite', 'spip_envois_ponctuels_liens', 'id_envois_ponctuel='.intval($id_objet));
-		
-		// Chaque auteur peut recevoir plusieurs exemplaires : 
-		// dupliquer les auteurs concernés dans le tableau 
+		$auteurs = sql_allfetsel('objet, id_objet, quantite', 'spip_envois_ponctuels_liens', 'id_envois_ponctuel='.$id_objet);
+
+		// Chaque auteur peut recevoir plusieurs exemplaires :
+		// dupliquer les auteurs concernés dans le tableau
 		$ids_auteurs = array();
 
 		foreach ($auteurs as $auteur) {
@@ -26,12 +29,17 @@ function action_exporter_envois_ponctuel($id_objet = null, $objet = null) {
 				$ids_auteurs[] = $auteur['id_objet'];
 			}
 		}
-		
+
 		if (count($ids_auteurs)) {
 			$exporter_auteur = charger_fonction('auteur', 'exporter');
 			$export = array();
-			
+
+			// $export = sql_select('id_auteur, nom, email, statut', 'spip_auteurs', sql_in('id_auteur', $ids_auteurs));
+
+			// $export = sql_allfetsel('id_auteur, nom, email, statut', 'spip_auteurs', sql_in('id_auteur', $ids_auteurs));
+
 			foreach ($ids_auteurs as $id_auteur) {
+				//$export[] = $exporter_auteur($id_auteur);
 				$export[] = array('descriptif' => '') + $exporter_auteur($id_auteur);
 			}
 		}
@@ -39,10 +47,8 @@ function action_exporter_envois_ponctuel($id_objet = null, $objet = null) {
 			$exporter_csv = charger_fonction('exporter_csv', 'inc');
 			$modele_entetes = charger_fonction('auteur_entetes', 'exporter');
 			$entetes = $modele_entetes();
-			$nom_fichier = 'envois-ponctuels_'.date('Ymd-His');
-			
-			$exporter_csv($nom_fichier, $export, ',', $entetes, $envoyer = true);
-			
+			$nom_fichier = 'envois_ponctuels_'.date('Ymd-His');
+			$exporter_csv($nom_fichier, $export, ',', $entetes);
 		}
 	}
 }
--- exporter/auteur.php
+++ exporter/auteur.php
@@ -12,15 +12,17 @@ function exporter_auteur_dist($id_auteur = 0) {
 	$export_contact = array();
 	$export_adresse = array();
 	$export_supplement = array();
-	
+
+	include_spip('inc/filtres');
+
 	if (!$id_auteur) {
 		return '';
 	}
-	
+
 	// contact
 	$contact = sql_fetsel('nom, prenom', 'spip_contacts', 'id_auteur='.$id_auteur);
 	$auteur = sql_fetsel('*', 'spip_auteurs', 'id_auteur='.$id_auteur);
-	
+
 	if ($contact) {
 		$export_contact['prenom'] = $contact['prenom'];
 		$export_contact['nom'] = $contact['nom'];
@@ -31,17 +33,16 @@ function exporter_auteur_dist($id_auteur = 0) {
 		$export_contact['nom'] = nom($auteur['nom']);
 		$export_supplement['email'] = ($auteur['email']) ? $auteur['email'] : '';
 	}
-	
+
 	// adresse
-	include_spip('inc/filtres');
 	$adresse_cles = array_flip(array('organisation', 'service', 'complement', 'voie', 'boite_postale', 'code_postal', 'ville', 'region', 'code_facteur', 'pays'));
-	
+
 	$adresse = sql_fetsel('*', 'spip_adresses AS adresses INNER JOIN spip_adresses_liens AS L1 ON (L1.id_adresse = adresses.id_adresse)', 'L1.id_objet='.$id_auteur.' AND L1.objet='.sql_quote('auteur'));
-	
+
 	$export_organisation['organisation'] = $adresse['organisation'];
 	$export_organisation['service'] = $adresse['service'];
 	unset($adresse_cles['organisation'], $adresse_cles['service']);
-	
+
 	if (
 		$export_contact['nom']
 		and $export_contact['prenom'] == ''
@@ -50,7 +51,7 @@ function exporter_auteur_dist($id_auteur = 0) {
 		$export_organisation['organisation'] = $export_contact['nom'];
 		$export_contact['nom'] = '';
 	}
-	
+
 	foreach ($adresse_cles as $cle => $v) {
 		if ($cle == 'pays' and $code_pays = $adresse[$cle]) {
 			$nom_pays = sql_getfetsel('nom', 'spip_pays', 'code='.sql_quote($code_pays));
@@ -60,29 +61,28 @@ function exporter_auteur_dist($id_auteur = 0) {
 		}
 		$export_adresse[$cle] = ($adresse[$cle]) ? $adresse[$cle] : '';
 	}
-	
+
 	$export = array_merge($export_organisation, $export_contact, $export_adresse, $export_supplement);
-	
-	
+
 	// Formater les données
 	$export['nom'] = (strlen($export['nom'])) ? mb_strtoupper($export['nom']) : '';
-	
+
 	$export['prenom'] = (strlen($export['prenom'])) ? ucwords($export['prenom']) : '';
-	
+
 	$export['organisation'] = (strlen($export['organisation'])) ? mb_strtoupper($export['organisation']) : '';
-	
+
 	$export['service'] = (strlen($export['service'])) ?  mb_strtoupper($export['service']) : '';
 
 	$export['complement'] = (strlen($export['complement'])) ? mb_strtoupper($export['complement']) : '';
-	
+
 	$export['voie'] = (strlen($export['voie'])) ? mb_strtoupper($export['voie']) : '';
-	
+
 	$export['boite_postale'] = (strlen($export['boite_postale'])) ? mb_strtoupper($export['boite_postale']) : '';
-	
+
 	$export['ville'] = (strlen($export['ville'])) ? mb_strtoupper($export['ville']) : '';
-	
+
 	$export['region'] = (strlen($export['region'])) ? mb_strtoupper($export['region']) : '';
-	
+
 	if (strlen($export['pays'])) {
 		$export['pays'] = ($export['pays'] == 'France') ? '' : mb_strtoupper($export['pays']);
 	}
